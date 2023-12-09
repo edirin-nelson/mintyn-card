ee42c5c84a43ef38fc145d37e5a4899210e7ea69
Edirin Nelson <nelsonedirin@gmail.com>
2023-12-09 23:29:28 +0100

    initial commit

---
+++ src/main/java/com/mintyn/mintyncard/MintynCardApplication.java
@@ -0,0 +1,13 @@
+package com.mintyn.mintyncard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MintynCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MintynCardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mintyn/mintyncard/MintynCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mintyn.mintyncard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MintynCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
